b7d747347abe00a4495e170587619c1360807cd3
Kirushik <velid5@mail.ru>
2024-03-31 19:59:30 +0300

    Moby was added

---
+++ resources/views/main.blade.php
@@ -0,0 +1,14 @@
+@extends('chipi_chapa.resources.views.layouts.app')
+
+@section('content')
+    <div class="container" style="text-align: center;">
+        <h1>KIRILL IVANOV</h1>
+    <div>
+@endsection
+
+{{--        <div class="container" style="text-align: center;">--}}
+{{--            <h1>KIRILL IVANOV</h1>--}}
+{{--            <div class="mb-3">--}}
+{{--                <a class="btn btn-primary" href="{{route('product.index')}}" role="button">GO TO REVIEW!</a>--}}
+{{--            </div>--}}
+{{--            <div>--}}
